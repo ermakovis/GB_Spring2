d09eb4c26f52b2182fbb7bd1611a107b86212ef9
ermakovis <ermakovis@gmail.com>
2021-02-13 09:02:27 +0300

    Lesson1.1
    Afterclass

--- src/main/java/ru/geekbrains/happy/market/controllers/OrderController.java
+++ src/main/java/ru/geekbrains/happy/market/controllers/OrderController.java
@@ -1,51 +1,31 @@
 package ru.geekbrains.happy.market.controllers;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.happy.market.beans.Cart;
 import ru.geekbrains.happy.market.model.Order;
-import ru.geekbrains.happy.market.model.OrderItem;
 import ru.geekbrains.happy.market.model.User;
-import ru.geekbrains.happy.market.repositories.OrderItemRepository;
 import ru.geekbrains.happy.market.repositories.OrderRepository;
 import ru.geekbrains.happy.market.repositories.UserRepository;
 
 import java.security.Principal;
-import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/order")
 @Data
 public class OrderController {
-    private Cart cart;
-    private OrderRepository orderRepository;
-    private OrderItemRepository orderItemRepository;
-    private UserRepository userRepository;
-
-    //Как-то в других контроллерах бех Autowire работает, но у меня без этого не стартует
-    @Autowired
-    public OrderController(Cart cart,
-                           OrderRepository orderRepository,
-                           OrderItemRepository orderItemRepository,
-                           UserRepository userRepository) {
-        this.cart = cart;
-        this.orderRepository = orderRepository;
-        this.orderItemRepository = orderItemRepository;
-        this.userRepository = userRepository;
-    }
+    private final Cart cart;
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
 
     @GetMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createOrder(Principal principal) {
-        List<OrderItem> items = cart.getItems();
         User user = userRepository.findByUsername(principal.getName()).orElseThrow();
-        Order order = new Order(user);
-        order = orderRepository.save(order);
-        for (OrderItem item : items) {
-            item.setOrder(order);
-        }
-        orderItemRepository.saveAll(items);
+        Order order = new Order(cart, user);
+        orderRepository.save(order);
     }
 }
--- src/main/java/ru/geekbrains/happy/market/model/Order.java
+++ src/main/java/ru/geekbrains/happy/market/model/Order.java
@@ -2,8 +2,15 @@ package ru.geekbrains.happy.market.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import ru.geekbrains.happy.market.beans.Cart;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Data
@@ -15,11 +22,28 @@ public class Order {
     @Column(name = "id")
     private Long id;
 
+    @OneToMany(mappedBy = "order")
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    private List<OrderItem> items;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    public Order(User user) {
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Order(Cart cart, User user) {
+        this.items = new ArrayList<>();
         this.user = user;
+        cart.getItems().forEach((oi) -> {
+            oi.setOrder(this);
+            items.add(oi);
+        });
     }
 }
